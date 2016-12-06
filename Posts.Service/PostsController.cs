93717b6c5f4deb418d868e1ac608611e5b7e872f
matt <matt@tune.zone>
2016-12-06 21:56:26 +0100

    LF-1: Initial commit

---
+++ Posts.Service/PostsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Posts.Service
+{
+    [RoutePrefix("api/posts")]
+    public class PostsController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
