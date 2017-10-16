ac9d8b7e842f627b0a0045a82860f69a9431fa0a
Matt <matthewfturner@gmail.com>
2017-10-17 01:30:38 +0200

    LF-6: Add an integration test

---
+++ Listings.Service.Tests/ListingsIntegrationTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Flurl;
+using Flurl.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Listings.Service.Tests
+{
+    [TestClass]
+    public class ListingsIntegrationTests
+    {
+        private readonly string _serviceUrl;
+
+        public ListingsIntegrationTests()
+        {
+            _serviceUrl = ConfigurationManager.AppSettings["ServiceUrl"];
+        }
+
+        [TestMethod]
+        public void GetAll_Succeeds()
+        {
+            var listings = _serviceUrl
+                .AppendPathSegments("api", "listings")
+                .GetJsonAsync<IList<Listing>>()
+                .Result;
+
+            Assert.IsNotNull(listings);
+            Assert.IsTrue(listings.Any());
+        }
+    }
+}
